0ad0a5343395a366728f2d631f73eea2a66bfe3e
furusin <furusin.oriver@gmail.com>
2020-06-17 23:24:30 +0900

    Add: startActivityForResult and onActivityResult

--- app/src/main/java/net/furusin/www/activityresultapisample/MainActivity.kt
+++ app/src/main/java/net/furusin/www/activityresultapisample/MainActivity.kt
@@ -1,10 +1,14 @@
 package net.furusin.www.activityresultapisample
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import net.furusin.www.activityresultapisample.databinding.ActivityMainBinding
+
+private const val REQUEST_CODE = 100
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -12,5 +16,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.navigationButton.setOnClickListener {
+            startActivityForResult(SubActivity.createIntent(this), REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_CODE -> {
+                    Toast.makeText(
+                        this,
+                        "result value = ${data?.getIntExtra(SubActivity.KEY_VALUE, 0)}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                else -> Unit // do nothing
+            }
+        }
     }
 }
