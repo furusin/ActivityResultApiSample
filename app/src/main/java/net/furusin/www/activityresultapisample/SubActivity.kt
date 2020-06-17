94f4b89bf3d41b0646ab965ac2fc1ac0d3342316
furusin <furusin.oriver@gmail.com>
2020-06-17 23:13:20 +0900

    Add: createIntent

--- app/src/main/java/net/furusin/www/activityresultapisample/SubActivity.kt
+++ app/src/main/java/net/furusin/www/activityresultapisample/SubActivity.kt
@@ -1,7 +1,10 @@
 package net.furusin.www.activityresultapisample
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import net.furusin.www.activityresultapisample.databinding.ActivitySubBinding
 
 class SubActivity : AppCompatActivity() {
@@ -9,6 +12,12 @@ class SubActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sub)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sub)
+    }
+
+    companion object {
+        const val KEY_VALUE = "KEY_VALUE"
+
+        fun createIntent(context: Context) = Intent(context, SubActivity::class.java)
     }
 }
