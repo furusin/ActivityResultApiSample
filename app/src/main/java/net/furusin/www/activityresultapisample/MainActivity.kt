67e82937097f5d83dacec679433a716d4854ad1f
furusin <furusin.oriver@gmail.com>
2020-06-19 09:04:34 +0900

    Add: use Activity Result API

--- app/src/main/java/net/furusin/www/activityresultapisample/MainActivity.kt
+++ app/src/main/java/net/furusin/www/activityresultapisample/MainActivity.kt
@@ -1,8 +1,12 @@
 package net.furusin.www.activityresultapisample
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
 import net.furusin.www.activityresultapisample.databinding.ActivityMainBinding
 
@@ -12,5 +16,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.navigationButton.setOnClickListener {
+            val startForResult =
+                registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult? ->
+                    if (result?.resultCode == Activity.RESULT_OK) {
+                        result.data?.let { data: Intent ->
+                            val value = data.getIntExtra(SubActivity.KEY_VALUE, 0)
+                            Toast.makeText(
+                                this,
+                                "result value = ${data.getIntExtra(SubActivity.KEY_VALUE, 0)}",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                }
+            startForResult.launch(SubActivity.createIntent(this))
+        }
+
     }
 }
